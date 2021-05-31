95e2a11a750d21ffc1fb1dfb01ac1e3dc82c40ef
Kirill Samusenko <kirill.samusenko.00@mail.ru>
2021-05-31 22:10:55 +0300

    More complex parser, fix parser errors
    Added animated loader, added error notification, added empty search fallback. Remove unused file.

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,83 +1,123 @@
 <template>
   <div class="search-page">
     <div class="uk-margin">
-      <form
-        class="uk-search uk-search-default search-form"
-        @submit.prevent="submitHandle"
-      >
-        <span
-          class="uk-search-icon-flip"
-          uk-search-icon
-        />
-        <input
-          v-model.trim="searchValue"
-          class="uk-search-input"
-          type="search"
-          placeholder="Search"
+      <form @submit.prevent="submitHandle">
+        <div class="uk-search uk-search-default search-form">
+          <span
+            class="uk-search-icon-flip"
+            uk-search-icon
+          />
+          <input
+            v-model.trim="searchValue"
+            autofocus
+            :class="`uk-input ${hasError ? 'uk-form-danger' : ''}`"
+            type="search"
+            placeholder="Search"
+          >
+        </div>
+        <button
+          class="uk-button uk-button-primary"
+          :disabled="searchValue.length === 0"
         >
+          Search
+        </button>
       </form>
-      <button
-        class="uk-button uk-button-default"
-        @click="submitHandle"
-      >
-        Search
-      </button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import UIkit from 'uikit/dist/js/uikit.min'
 
 @Component({ name: 'SearchPage' })
 export default class Search extends Vue {
   private searchValue = ''
+  private hasError = false
+
+  @Watch('searchValue')
+  private searchValueChange () {
+    this.hasError = false
+    UIkit.notification.closeAll()
+  }
+
+  private searchTypes = [
+    {
+      searchType: 'email',
+      normalize: (text: string): string => {
+        return text
+      },
+      validation: (text: string): boolean => {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          .test(text)
+      }
+    },
+    {
+      searchType: 'name',
+      normalize: (text: string): string => {
+        return text.replace(' ', '_')
+      },
+      validation: (text: string): boolean => {
+        const indexOfSpace = text.indexOf(' ')
+        return indexOfSpace > -1 && text.length > 3 && !/[^a-zA-Z0-9]/.test(text.replace(' ', ''))
+      }
+    },
+    {
+      searchType: 'ip',
+      normalize: (text: string): string => {
+        return text
+      },
+      validation: (text: string): boolean => {
+        return (/^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/).test(text)
+      }
+    },
+    {
+      searchType: 'phone',
+      normalize: (text: string): string => {
+        return text.replace(/\D/g, '')
+      },
+      validation: (text: string): boolean => {
+        return /^[+]?[0-9]?[\s]?[(]?[0-9]{3}[)]?[-\s]?[0-9]{3}[-\s]?[0-9]{2,4}[-\s]?[0-9]{0,2}$/im
+          .test(text)
+      }
+    },
+    {
+      searchType: 'nickname',
+      normalize: (text: string): string => {
+        return text[0] === '@' ? text : `@${text}`
+      },
+      validation: (text: string): boolean => {
+        return text.length > 2 && !/[^a-zA-Z0-9]/.test(text)
+      }
+    }
+  ]
 
   private submitHandle (): void {
-    const indexOfEmailSign = this.searchValue.indexOf('@')
-    // Zero because nickname can start on "@"
+    let typeOverlap = false
 
-    let value = this.searchValue
-    if (indexOfEmailSign > 0) {
-      this.$router.push(`email/${value}`)
-      return
-    }
-
-    const nameCheck = !/[^a-zA-Z0-9]/.test(this.searchValue.split(' ').join(' '))
-    if (indexOfEmailSign === 0 || nameCheck) {
-      if (indexOfEmailSign !== 0) {
-        value = '@' + value
+    for (let i = 0; i < this.searchTypes.length; i++) {
+      if (this.searchTypes[i].validation(this.searchValue)) {
+        typeOverlap = true
+        this.$router.push(`${this.searchTypes[i].searchType}/${this.searchTypes[i].normalize(this.searchValue)}`)
+        break
       }
-      this.$router.push(`nickname/${value}`)
-      return
     }
-
-    const indexOfSpace = this.searchValue.indexOf(' ')
-    if (nameCheck && indexOfSpace > -1) {
-      this.$router.push(`full-name/${value}`)
-      return
-    }
-
-    const IPCheck = (/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/).test(this.searchValue)
-    if (IPCheck) {
-      this.$router.push(`ip/${value}`)
-      return
-    }
-
-    const phoneCheck = /^[+]?[0-9]?[\s]?[(]?[0-9]{3}[)]?[-\s]?[0-9]{3}[-\s]?[0-9]{2,4}[-\s]?[0-9]{0,2}$/im
-      .test(this.searchValue)
-    if (phoneCheck) {
-      value = value
-        .replace(/\D/g, '')
-      this.$router.push(`phone/${value}`)
-      return undefined
+    // If type hasn't overlap
+    if (!typeOverlap) {
+      this.hasError = true
+      UIkit.notification({
+        message: 'Parsing Error',
+        status: 'danger',
+        pos: 'top-center',
+        timeout: 5000
+      })
     }
   }
 }
 </script>
 <style>
 .search-page {
-  height: 100vh;
+  min-height: 100vh;
   width: 100%;
   display: flex;
   justify-content: center;
